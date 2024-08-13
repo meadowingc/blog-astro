de4c4e18ef95d9905bfe06edea04a5f8c2480be1
Meadow <meadowingc@gmail.com>
2024-08-13 17:47:58 -0600

    more sitemap goodness

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
+// https://docs.astro.build/en/guides/integrations-guide/sitemap/
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
