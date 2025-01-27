172938510f54f07b55e2157a2c3f65ccc35521fc
Meadow <meadowingc@gmail.com>
2025-01-27 14:26:40 -0600

    per-tag rss feed

--- src/pages/feeds/index.md
+++ src/pages/feeds/index.md
@@ -7,3 +7,5 @@ I have a couple of feeds, each displaying different things.
 
 - [Main/Complete Feed](/feeds/main_rss.xml) — This tracks everything that I publish on the site (blog/journal)
 - [Blog Posts Feed](/feeds/blog_rss.xml) — This tracks only the blog posts
+
+There's also one feed for each tag that I use on the site. You can find them on the actual [tags page](/tags).
---
+++ src/pages/feeds/tag_feed_[tagName].xml.ts
@@ -0,0 +1,40 @@
+// FILE: src/pages/feeds/[tag].xml.ts
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export const getStaticPaths = async () => {
+  const allPosts = (await getCollection("obsidianPublishedPosts"))
+    .map((col) => col.data)
+    .sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+
+  const allTags = allPosts.flatMap((post) => post.tags).sort();
+
+  const tags = Array.from(new Set(allTags));
+
+  return tags.map((tag) => {
+    const postsWithTag = allPosts.filter((post) => post.tags.includes(tag));
+    return {
+      params: { tagName: `${tag}` },
+      props: { posts: postsWithTag, tag: tag },
+    };
+  });
+};
+
+export async function GET({ params, site , props }) {
+  const tag = params.tagName;
+  const posts = props.posts;
+
+  return rss({
+    title: `Meadow - Posts tagged with ${tag}`,
+    description: `Posts tagged with ${tag} on Meadow`,
+    site: site,
+    stylesheet: "/rss/pretty-feed-v3.xsl",
+    items: posts.map((post) => ({
+      title: post.title,
+      link: `/blog/${post.slug}/`,
+      pubDate: post.publishedAt,
+      description: post.body,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
